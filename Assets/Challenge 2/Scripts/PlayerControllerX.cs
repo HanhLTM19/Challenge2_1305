de0b25a9eaa8e94e5a777d26c1c38e9ffba459cb
Le Thi My Hanh <hanhltm19@gmail.com>
2022-05-13 18:16:50 +0700

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float dogTime;
+    float m_dogTime;
+    void Start()
+    {
+        m_dogTime = 0;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        m_dogTime -= Time.deltaTime;
+        if (m_dogTime <= 0)
+        {
+            SpawnDog();
+        }
+
+    }
+    public void SpawnDog()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_dogTime = dogTime;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+        }
+    }
+}
+  
